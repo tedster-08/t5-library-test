79868b02d8b8291cb9924acb23da109fb96c62fb
tedster-08 <75181949+tedster-08@users.noreply.github.com>
2023-04-12 22:13:38 -0400

    update gitignore

--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
-from flask import render_template, Blueprint, request, jsonify
+from flask import render_template, Blueprint, request, jsonify, redirect
 import json
+import csv
+import os
 from .models import MeritBadgePamphlet
 from . import db
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
 
 @views.route("/")
@@ -13,43 +15,50 @@ def home():
 
 @views.route("/library")
 def library():
-    books = MeritBadgePamphlet.query.order_by(
-        MeritBadgePamphlet.title).all()
+    books = MeritBadgePamphlet.query.order_by(MeritBadgePamphlet.title).all()
     print(books)
     return render_template("library.html", books=books)
+
+
+def _is_eagle_required(book):
+
+    eagle_required_badges = [
+        "First Aid",
+        "Citizenship in the Community",
+        "Citizenship in the Nation",
+        "Citizenship in Society",
+        "Citizenship in the World",
+        "Communication",
+        "Cooking",
+        "Personal Fitness",
+        "Emergency Preparedness",
+        "Lifesaving",
+        "Environmental Science",
+        "Sustainability",
+        "Personal Management",
+        "Swimming",
+        "Hiking",
+        "Cycling",
+        "Camping",
+        "Family Life",
+    ]
+
+    return book in eagle_required_badges
 
 
 @views.route("/add-book", methods=["GET", "POST"])
 def add_book():
     if request.method == "POST":
-
-        eagle_required_badges = [
-            "First Aid",
-            "Citizenship in the Community",
-            "Citizenship in the Nation",
-            "Citizenship in Society",
-            "Citizenship in the World",
-            "Communication",
-            "Cooking",
-            "Personal Fitness",
-            "Emergency Preparedness",
-            "Lifesaving",
-            "Environmental Science",
-            "Sustainability",
-            "Personal Management",
-            "Swimming",
-            "Hiking",
-            "Cycling",
-            "Camping",
-            "Family Life"
-        ]
-
         title = request.form.get("title")
-        is_up_to_date = (request.form.get("indate") == "on")
-        is_eagle_required = (title in eagle_required_badges)
+        is_up_to_date = request.form.get("indate") == "on"
+        is_eagle_required = _is_eagle_required(title)
 
         new_pamphlet = MeritBadgePamphlet(
-            title=title, is_up_to_date=is_up_to_date, is_eagle_required=is_eagle_required, is_checked_out=False)
+            title=title,
+            is_up_to_date=is_up_to_date,
+            is_eagle_required=is_eagle_required,
+            is_checked_out=False,
+        )
         db.session.add(new_pamphlet)
         db.session.commit()
     return render_template("add_book.html")
@@ -87,9 +96,9 @@ def librarian():
 
 
 def _shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
+    func = request.environ.get("werkzeug.server.shutdown")
     if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
+        raise RuntimeError("Not running with the Werkzeug Server")
     func()
 
 
@@ -103,3 +112,38 @@ def shutdown_server_from_url():
 @views.route("/libauth")
 def libauth():
     return render_template("libauth.html")
+
+
+@views.route("/upload", methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        if "file" not in request.files:
+            print("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        if file.filename == "":
+            print("No selected file")
+            return redirect(request.url)
+        if file:
+            file.save("temp-data.csv")
+            with open("temp-data.csv", "r") as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    next(reader)
+                    title, in_date, checked_out, checked_out_to = row
+                    in_date = False if in_date == 0 else True
+                    checked_out = False if checked_out == 0 else True
+                    new_pamphlet = MeritBadgePamphlet(
+                        title=title,
+                        is_up_to_date=in_date,
+                        is_eagle_required=_is_eagle_required(
+                            title),
+                        is_checked_out=checked_out,
+                        checked_out_to=checked_out_to,
+                    )
+
+                    db.session.add(new_pamphlet)
+        db.session.commit()
+        os.remove("temp-data.csv")
+
+    return render_template("upload.html")
